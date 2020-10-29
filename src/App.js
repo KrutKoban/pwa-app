bc0da1d6102d86f491cf2c42ce11435416c8be5b
Valery.Nikiforov <valery.nikiforov@MacBook-Pro-X5.local>
2020-10-29 22:06:23 +0300

    second first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'
+      >
+        <a-entity gltf-model="../assets/witness.png" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: 37.6173; latitude: 55.755826;" animation-mixer/>
+        <a-camera gps-camera rotation-reader></a-camera>
+      </a-scene>
     </div>
   );
-}
+};
 
 export default App;
